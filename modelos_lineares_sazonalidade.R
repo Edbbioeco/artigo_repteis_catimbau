4df52142338a7cd8d5ab0b3c50c6382295ad3a10
Edbbioeco <edsonbbiologia@gmail.com>
2026-01-30 10:00:53 -0300

    Script para os modelos lineares

--- modelos_lineares_sazonalidade.R
+++ modelos_lineares_sazonalidade.R
@@ -20,41 +20,15 @@ library(ggview)
 
 # Dados ----
 
-## Composição de espécies ----
-
-### Importando ----
+## Importando ----
 
 comp <- readxl::read_xls("composicao.xls")
 
-### Visualizando -----
+## Visualizando -----
 
 comp
 
 comp |> dplyr::glimpse()
-
-## Variáveis macroclimáticas ----
-
-### Importando ----
-
-var_micro <- readxl::read_xls("var_micro.xls")
-
-### Visualizando -----
-
-var_micro
-
-var_micro |> dplyr::glimpse()
-
-## Variáveis microclimáticas ----
-
-### Importando ----
-
-var_macro <- readxl::read_xls("var_macro.xls")
-
-### Visualizando -----
-
-var_macro
-
-var_macro |> dplyr::glimpse()
 
 # Riqueza e diversidade ----
 
