b36fd160062eb0d9e894435f793423468c341805
Edbbioeco <edsonbbiologia@gmail.com>
2026-01-31 10:24:49 -0300

    Script para o teste de Mantel por sazonalidade

--- mantel_sazonalidade.R
+++ mantel_sazonalidade.R
@@ -35,3 +35,11 @@ coord <- readxl::read_xlsx("coord_trat.xlsx")
 coord |> as.data.frame()
 
 coord |> dplyr::glimpse()
+
+# Dissimilaridade da composição ----
+
+dis_bray <- comp |>
+  tibble::column_to_rownames(var = "Parcela") |>
+  vegan::vegdist()
+
+dis_bray
