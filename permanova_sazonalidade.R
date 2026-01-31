0ae4e901c1e18874bddd21ce5c591ce67b5e0b77
Edbbioeco <edsonbbiologia@gmail.com>
2026-01-31 09:26:21 -0300

    Script para a PERMANOVA de sazonalidade

---
+++ permanova_sazonalidade.R
@@ -0,0 +1,31 @@
+# Pacotes ----
+
+library(readxl)
+
+library(tidyverse)
+
+library(vegan)
+
+library(ggvegan)
+
+library(ggtext)
+
+library(ggview)
+
+# Dados ----
+
+## Importando ----
+
+## Visualizando ----
+
+# PERMANOVA ----
+
+## Calculando PERMANOVA ----
+
+## Estatísticas da PERMONOVA ----
+
+# NMDS ----
+
+## Calculando NMDS ----
+
+## Gráfico ----
